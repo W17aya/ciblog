f577885937d055efc7d9b6729e5f99800d30c60f
Eko Wijaya <32318250+W17aya@users.noreply.github.com>
2018-08-15 22:56:59 +0800

    Mark_4

--- application/views/cPanel/beranda.php
+++
@@ -1,5 +0,0 @@
-<h3 class="judul">Selamat Datang..</h3>
-<span class="isi">
-Hai <b><?php echo $nama; ?></b>, Selamat datang di halaman Administrator. Anda login dengan username <b><?php echo $username; ?></b>. Untuk mengelola konten web, silahkan klik menu sebelah kiri. <br />
-Perlu diperhatikan!! Apabila Anda ingin keluar system, harap klik <b><?php echo anchor('c_admin/logout', 'Logout'); ?></b> untuk menjaga keamanan system web ini. Terima kasih..
-</span>
